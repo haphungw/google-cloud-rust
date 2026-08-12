968bea34e6eb4fa537f04b16c9787652697c96c8
haphungw <haphung@google.com>
2026-08-12 18:42:37 +0000

    refactor: exercise more job parameters

--- src/bigquery/examples/src/job/create_job.rs
+++ src/bigquery/examples/src/job/create_job.rs
@@ -15,15 +15,19 @@
 // [START bigquery_create_job]
 use google_cloud_bigquery_v2::client::JobService;
 use google_cloud_bigquery_v2::model::{Job, JobConfiguration, JobConfigurationQuery};
+use std::collections::HashMap;
 
 pub async fn sample(project_id: &str) -> anyhow::Result<String> {
     let job_service = JobService::builder().build().await?;
     let job = Job::new().set_configuration(
-        JobConfiguration::new().set_query(
-            JobConfigurationQuery::new()
-                .set_query("SELECT 1")
-                .set_use_legacy_sql(false),
-        ),
+        JobConfiguration::new()
+            .set_labels(HashMap::from([("example-label".to_string(), "example-value".to_string())]))
+            .set_query(
+                JobConfigurationQuery::new()
+                    .set_query("SELECT 1")
+                    .set_use_legacy_sql(false)
+                    .set_maximum_bytes_billed(10000000_i64),
+            ),
     );
 
     let job = job_service
@@ -36,7 +40,6 @@ pub async fn sample(project_id: &str) -> anyhow::Result<String> {
     let job_id = job.job_reference.unwrap().job_id;
     println!("Job completed successfully: {}", job_id);
 
-    println!("Job completed successfully.");
     Ok(job_id)
 }
 // [END bigquery_create_job]
